06428a487273c6f7bc182a610b5c1d69fe206045
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-02-13 13:01:25 +0900

    strcut

--- main.go
+++ main.go
@@ -5,12 +5,36 @@ import (
 	"fmt"
 	"log"
 	"os/exec"
+	"time"
 )
 
 type Employee struct {
+	ID        int
+	Name      string
+	Address   string
+	DoB       time.Time
+	Position  string
+	Salary    int
+	ManagerID int
+}
+
+type Point struct {
+	x, Y int
 }
 
 func main() {
+	var dilbert Employee
+	dilbert.Salary -= 5000
+	fmt.Printf("%v\n", dilbert)
+	fmt.Printf("%+v\n", dilbert)
+	position := dilbert.Position
+	position = "Senior"
+	fmt.Printf("%+v\n", position)
+	fmt.Printf("%+v\n", dilbert)
+
+	var p = Point{2, 2}
+	fmt.Printf("%v", p)
+
 	cmd := exec.Command("./script.sh")
 	var stdout bytes.Buffer
 	var stderr bytes.Buffer
