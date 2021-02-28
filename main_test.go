f85981319cde651061c10b936b9cc532c252352b
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-03-01 01:37:54 +0900

    allocation total validation

--- main_test.go
+++ main_test.go
@@ -11,3 +11,8 @@ func TestAbs(t *testing.T) {
 		t.Errorf("Abs(-1) = %f; want 1", got)
 	}
 }
+
+func validateAllocatedBudgetsTotal(budget float64, allocatedBudgets []float64) bool {
+	allocatedTotal := CalculateTotal(allocatedBudgets)
+	return math.Abs((budget-allocatedTotal)/budget) < 0.000001
+}
